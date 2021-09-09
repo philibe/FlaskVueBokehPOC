4a1ecf2dc2bc2db2fe81beb6376496eaad59748f
phili_b <philippe@colibri-informatique.com>
2021-09-09 17:58:25 +0200

    frontend/src/api/apisinus.js: drop commented code

--- frontend/src/api/apisinus.js
+++ frontend/src/api/apisinus.js
@@ -1,24 +1,6 @@
 import axios from "axios";
 
 export default {
-  // factoriser éventuellement le axios.get(),
-  // attention gestion de paramètres non présent dans cet exemple
-  apiGetProductsFlask(callback) {
-    axios
-      .get("/api/products")
-      .then((response) => {
-        console.log(response.data);
-        callback(response.data.products);
-      })
-      .catch((err) =>
-        console.log(
-          (process.env.NODE_ENV || "dev") == "build"
-            ? err.message
-            : JSON.stringify(err)
-        )
-      );
-  },
-
   apiGetBokehinlinejs(callback) {
     axios
       .get("/api/bokehinlinejs")
